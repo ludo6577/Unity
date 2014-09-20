a868b4f6cc5b9d4731b6d8c9e001d887ef1de061
ludo6577 <ludo6577@gmail.com>
2014-09-20 21:00:59 +0200

    weapon

---
+++ Assets/hit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hit : MonoBehaviour {
+
+	public GameObject weapon;
+
+	public AnimationCurve curveX;
+	public AnimationCurve curveY;
+	public AnimationCurve curveZ;
+	public float maxTime;
+
+	private float index = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		index += Time.deltaTime;
+		if (index > maxTime)
+			index = 0;
+		Vector3 vecPos = new Vector3 (curveX.Evaluate (index), curveY.Evaluate (index), curveZ.Evaluate (index));
+		weapon.transform.localPosition = vecPos;
+	}
+}
